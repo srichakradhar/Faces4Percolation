8ab44d0c212d8e0b9e4e77cf5db7a3e546909786
Nagireddy Srichakradhar Reddy <srichakra3nsr3@gmail.com>
2018-08-25 21:35:59 +0530

    update rgb2array
    Change threshold to 150 from 250

--- rgb2array.py
+++ rgb2array.py
@@ -3,16 +3,25 @@ from PIL import Image
 from random import shuffle
 
 N = 50
-img = Image.open(r"C:\Users\sridhar\Pictures\percolate\sheebuBW_50_new.bmp")
-out = list(map(lambda x: map(lambda y: 1 if np.average(y) > 250 else 0, x), np.array(img)))
-open_sites = [N]
+filename = input("Filename: ")
+extension = input("Extension: ")
+img = Image.open("C:\\Users\\sridhar\\Pictures\\percolate\\" + filename + '.' + extension)
+im_ar = np.array(img)
+##out = list(map(lambda x: map(lambda y: 1 if np.average(y) > 150 else 0, x), im_ar))
+
+open_sites = []
 for i in range(N):
-    row = list(out[i])
     for j in range(N):
-        if row[j] == 1:
-            open_sites.append(str(i+1) + " " + str(j+1) + "\n")
+        if np.average(im_ar[i][j]) > 150:
+            open_sites.append(str(i) + " " + str(j) + "\n")
+
+##for i in range(N):
+##    row = list(out[i])
+##    for j in range(N):
+##        if row[j] == 1:
+##            open_sites.append(str(i) + " " + str(j) + "\n")
 
 shuffle(open_sites)
-with open("sheebu" + str(N) + ".txt",'w',encoding = 'utf-8') as f:
+with open(filename + "_" + str(N) + ".txt",'w',encoding = 'utf-8') as f:
     f.write(str(N) + "\n")
     f.writelines(map(str, open_sites))
