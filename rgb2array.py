59d3d75bf9ed701a93dfde825bc75e2ceaf811c2
Nagireddy Srichakradhar Reddy <srichakra3nsr3@gmail.com>
2018-08-25 19:47:52 +0530

    rgb2array
    python script to convert b/w image to index file

---
+++ rgb2array.py
@@ -0,0 +1,18 @@
+import numpy as np
+from PIL import Image
+from random import shuffle
+
+N = 50
+img = Image.open(r"C:\Users\sridhar\Pictures\percolate\sheebuBW_50_new.bmp")
+out = list(map(lambda x: map(lambda y: 1 if np.average(y) > 250 else 0, x), np.array(img)))
+open_sites = [N]
+for i in range(N):
+    row = list(out[i])
+    for j in range(N):
+        if row[j] == 1:
+            open_sites.append(str(i) + " " + str(j) + "\n")
+
+shuffle(open_sites)
+with open("sheebu" + str(N) + ".txt",'w',encoding = 'utf-8') as f:
+    f.write(str(N) + "\n")
+    f.writelines(map(str, open_sites))
